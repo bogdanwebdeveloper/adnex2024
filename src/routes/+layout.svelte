7a202362fd515ed7c39900b36b02073c01d08858
bogdanwebdeveloper <bogdan.nnnnnnn1@gmail.com>
2024-07-30 08:38:48 +0300

    a

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import modeobserver from '$lib/modeobserver';
 	import { onMount } from 'svelte';
 	import Runatics from './utils/Runatics.svelte';
+  // @ts-ignore
   export let data;
+  // @ts-ignore
   const analyticsId = data.ANALYTICS_ID
 
 	onMount(modeobserver);
